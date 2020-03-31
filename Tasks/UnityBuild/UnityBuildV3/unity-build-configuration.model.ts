dde6e31cdac6bfbbadcfb7264430765c4a2243ab
Dino Fejzagic <dino.f@live.de>
2020-03-31 14:53:26 +0200

    Fix type errors

--- Tasks/UnityBuild/UnityBuildV3/unity-build-configuration.model.ts
+++ Tasks/UnityBuild/UnityBuildV3/unity-build-configuration.model.ts
@@ -14,13 +14,13 @@ export interface UnityBuildConfiguration {
      * Path to the Unity project to build, relative to repository root.
      * If empty or undefined, defaults to repository root.
      */
-    projectPath: string | undefined;
+    projectPath: string;
 
     /**
      * Build output path. This can be relative to repository root or fully qualified.
      * If empty or undefined, defaults to repository root.
      */
-    outputPath: string | undefined;
+    outputPath: string;
 
     /**
      * Output filename, used e.g as drop.exe, drop.app, drop.apk.
