b970481e6fd63d6a17b0148ceaeb6781a0f6b194
Dino Fejzagic <dino.f@live.de>
2020-04-01 10:06:38 +0200

    Implement improved error handling in version helper package

--- Tasks/Common/ProjectVersion/lib/project-version.model.ts
+++ Tasks/Common/ProjectVersion/lib/project-version.model.ts
@@ -4,9 +4,14 @@
 export interface UnityProjectVersion {
 
     /**
-     * Project version identifier.
+     * Project version identifier, e.g. 2019.3.5f1.
      */
     version: string;
+
+    /**
+     * Project version identifier with revision, e.g. 2019.3.5f1 (d691e07d38ef).
+     */
+    versionWithRevision: string;
 
     /**
      * Is the project using a beta version of Unity?
@@ -17,4 +22,9 @@ export interface UnityProjectVersion {
      * Is the project using an alpha version of Unity?
      */
     isAlpha: boolean;
+
+    /**
+     * Optional error description in case an error occured while determining the project version.
+     */
+    error?: string;
 }
