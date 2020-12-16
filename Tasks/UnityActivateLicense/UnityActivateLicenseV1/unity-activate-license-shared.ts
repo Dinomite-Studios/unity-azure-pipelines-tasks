edaf4041d4b57652a388eb75b7bc143a4a9246f0
Dino Fejzagić <dino.f@live.de>
2020-12-16 08:02:35 +0100

    Update unity activate license dependencies

--- Tasks/UnityActivateLicense/UnityActivateLicenseV1/unity-activate-license-shared.ts
+++ Tasks/UnityActivateLicense/UnityActivateLicenseV1/unity-activate-license-shared.ts
@@ -1,21 +1,24 @@
 import tl = require('azure-pipelines-task-lib/task');
-import { ProjectVersionService, UnityProjectVersion } from '@dinomite-studios/unity-project-version';
+import {
+    ProjectVersionService,
+    UnityProjectVersion
+} from '@dinomite-studios/unity-project-version';
 
 export async function getUnityEditorVersion(): Promise<UnityProjectVersion> {
     const projectPath = tl.getPathInput('unityProjectPath') || '';
-    console.log(`${tl.loc('ProjectPathInfo')} ${projectPath}`);
+    console.log(`${tl.loc('projectPathInfo')} ${projectPath}`);
 
     const unityVersion = await ProjectVersionService.determineProjectVersionFromFile(projectPath);
     if (unityVersion.error) {
-        const error = `${tl.loc('FailGetUnityEditorVersion')} | ${unityVersion.error}`;
+        const error = `${tl.loc('failGetUnityEditorVersion')} | ${unityVersion.error}`;
         console.error(error);
         throw new Error(error);
     }
 
-    const successGetVersionLog = `${tl.loc('SuccessGetUnityEditorVersion')} ${unityVersion.version}, alpha=${unityVersion.isAlpha}, beta=${unityVersion.isBeta}`;
+    const successGetVersionLog = `${tl.loc('successGetUnityEditorVersion')} ${unityVersion.version}, alpha=${unityVersion.isAlpha}, beta=${unityVersion.isBeta}`;
     console.log(successGetVersionLog);
     if (unityVersion.isAlpha || unityVersion.isBeta) {
-        console.warn(tl.loc('WarningAlphaBetaVersion'));
+        console.warn(tl.loc('warningAlphaBetaVersion'));
     }
 
     return unityVersion;
