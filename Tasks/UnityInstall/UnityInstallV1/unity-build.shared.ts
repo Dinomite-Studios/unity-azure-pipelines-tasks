93245131049923b3baacdcb09fdbad32d3da839a
Dino Fejzagić <dino.f@live.de>
2022-12-17 23:44:31 +0100

    Update pipelines task lib and fix compiler error

---
+++ Tasks/UnityInstall/UnityInstallV1/unity-build.shared.ts
@@ -0,0 +1,23 @@
+import tl = require('azure-pipelines-task-lib/task');
+import { UnityVersionInfoResult, UnityVersionTools } from '@dinomite-studios/unity-azure-pipelines-tasks-lib/';
+
+export function getUnityEditorVersion(): UnityVersionInfoResult  {
+    const projectPath = tl.getPathInput('unityProjectPath') || '';
+    console.log(`${tl.loc('projectPathInfo')} ${projectPath}`);
+
+    const unityVersion = UnityVersionTools.determineProjectVersionFromFile(projectPath);
+    if (unityVersion.error) {
+        const error = `${tl.loc('failGetUnityEditorVersion')} | ${unityVersion.error}`;
+        console.error(error);
+        throw new Error(error);
+    }
+
+    const successGetVersionLog = `${tl.loc('successGetUnityEditorVersion')} ${unityVersion.info!.version}${unityVersion.info!.revision ? `, revision=${unityVersion.info!.revision}` : ''}, alpha=${unityVersion.info!.isAlpha}, beta=${unityVersion.info!.isBeta}`;
+    console.log(successGetVersionLog);
+
+    if (unityVersion.info!.isAlpha || unityVersion.info!.isBeta) {
+        console.warn(tl.loc('warningAlphaBetaVersion'));
+    }
+
+    return unityVersion;
+}
--- Tasks/UnityInstall/UnityInstallV1/unity-install.ts
+++ Tasks/UnityInstall/UnityInstallV1/unity-install.ts
@@ -1,9 +1,11 @@
 import path = require('path');
 import tl = require('azure-pipelines-task-lib/task');
+import { getUnityEditorVersion } from './unity-build.shared';
 
 tl.setResourcePath(path.join(__dirname, 'task.json'));
 
 // Input variables.
+const versionSelectionModeVariableName = "versionSelectionMode";
 const versionInputVariableName = 'version';
 const revisionInputVariableName = 'revision';
 
@@ -13,8 +15,19 @@ const revisionInputVariableName = 'revision';
 function run() {
     try {
         // Setup and read inputs.
-        const version = tl.getPathInput(versionInputVariableName, true);
-        const revision = tl.getPathInput(revisionInputVariableName, true);
+        const versionSelectionMode = tl.getInput(versionSelectionModeVariableName, true)!
+        var version = '';
+        var revision = '';
+
+        if (versionSelectionMode === 'specify') {
+            version = tl.getPathInput(versionInputVariableName, true)!;
+            revision = tl.getPathInput(revisionInputVariableName, true)!;
+        } else {
+            const projectVersion = getUnityEditorVersion();
+            version = projectVersion.info!.version;
+            revision = projectVersion.info!.revision!;
+        }
+
         console.log(`${tl.loc('installVersionInfo')} ${version} (${revision})`);
 
         // Set task result succeeded.
@@ -25,7 +38,7 @@ function run() {
             tl.setResult(tl.TaskResult.Failed, e.message);
         } else {
             console.error(e);
-            tl.setResult(tl.TaskResult.Failed, e);
+            tl.setResult(tl.TaskResult.Failed, `${e}`);
         }
     }
 }
