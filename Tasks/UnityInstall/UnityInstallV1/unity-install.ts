ddaebc2686167bb7e7df9a140061067bf2591943
Dino Fejzagić <dino.f@live.de>
2022-12-18 00:44:42 +0100

    Add parameters for module install

--- Tasks/UnityInstall/UnityInstallV1/unity-install.ts
+++ Tasks/UnityInstall/UnityInstallV1/unity-install.ts
@@ -8,6 +8,13 @@ tl.setResourcePath(path.join(__dirname, 'task.json'));
 const versionSelectionModeVariableName = "versionSelectionMode";
 const versionInputVariableName = 'version';
 const revisionInputVariableName = 'revision';
+const androidModuleInputVariableName = 'installAndroidModule';
+const iOSModuleInputVariableName = 'installIOSModule';
+const tvOSModuleInputVariableName = 'installTVOSModule';
+const macMonoModuleInputVariableName = 'installMacMonoModule';
+const windowsModuleInputVariableName = 'installWindowsIL2CPPModule';
+const uwpModuleInputVariableName = 'installUWPModule';
+const webGLModuleInputVariableName = 'installWebGLModule';
 
 /**
  * Main task runner. Executes the task and sets the result status for the task.
@@ -31,15 +38,42 @@ function run() {
 
         console.log(`${tl.loc('installVersionInfo')} ${version} (${revision})`);
 
-        const unityHubCmd = tl.tool(unityHubExecutablePath)
+        const installEditorCmd = tl.tool(unityHubExecutablePath)
             .arg('--')
             .arg('--headless')
             .arg('install')
             .arg('--version').arg(version)
             .arg('--changeset').arg(revision);
+        installEditorCmd.exec();
 
-        console.log(`${unityHubCmd}`)
-        unityHubCmd.exec();
+        const installModulesCmd = tl.tool(unityHubExecutablePath)
+            .arg('--')
+            .arg('--headless')
+            .arg('install-modules')
+            .arg('--version').arg(version)
+            .arg('--childModules')
+            .arg('--module');
+        installModulesCmd.exec();
+
+        //  Android Build Support: android
+        //           Android SDK & NDK Tools: android-sdk-ndk-tools
+        //           OpenJDK: android-open-jdk
+        //           Microsoft Visual Studio Community 2017/2019: visualstudio
+        //           iOS Build Support: ios
+        //           tvOS Build Support: appletv
+        //           Linux Build Support: linux
+        //           Linux Build Support (Mono): linux-mono
+        //           Linux Build Support (IL2CPP): linux-il2cpp
+        //           Mac Build Support (Mono): mac-mono
+        //           Windows Build Support (IL2CPP): windows-il2cpp
+        //           Universal Windows Platform Build Support: universal-windows-platform
+        //           UWP Build Support (IL2CPP): uwp-il2cpp
+        //           UWP Build Support (.NET): uwp-.net
+        //           WebGL Build Support: webgl
+        //           Lumin OS (Magic Leap) Build Support: lumin
+        //           Facebook Gameroom: facebookgameroom
+        //           Facebook Gameroom Build Support: facebook-games
+        //           Vuforia Augmented Reality Support: vuforia-ar
 
         // Set task result succeeded.
         tl.setResult(tl.TaskResult.Succeeded, tl.loc('successUnityInstall'));
