0d5dc6386681e221104f700b77b1b6dc65dcc160
Dino Fejzagić <dino.f@live.de>
2024-12-09 15:05:29 +0100

    Fix tests

--- Tasks/UnityGetProjectVersion/UnityGetProjectVersionV1/test/_suite.ts
+++ Tasks/UnityGetProjectVersion/UnityGetProjectVersionV1/test/_suite.ts
@@ -2,66 +2,71 @@ import assert from 'assert';
 import * as mocktest from 'azure-pipelines-task-lib/mock-test';
 import * as path from 'path';
 
-describe("Unity Get Project Version", async () => {
-    it("Error determining the project version from file", async (done) => {
+describe("Unity Get Project Version", () => {
+    it("Error determining the project version from file", (done) => {
         let testPath = path.join(__dirname, 'errorDeterminingTheProjectVersionFromFile.js');
 
         let runner: mocktest.MockTestRunner = new mocktest.MockTestRunner(testPath);
 
-        await runner.runAsync();
+        runner.runAsync()
+            .then(() => {
+                assert.strictEqual(runner.failed, true);
+                assert.strictEqual(runner.invokedToolCount, 0);
+                assert(runner.stdOutContained('loc_mock_failGetUnityEditorVersion | Unknown project version format encountered'));
 
-        assert.strictEqual(runner.failed, true);
-        assert.strictEqual(runner.invokedToolCount, 0);
-        assert(runner.stdOutContained('loc_mock_failGetUnityEditorVersion | Unknown project version format encountered'));
-
-        done();
+                done();
+            }
+            );
     })
 
-    it("Success (alpha) determining the project version from file", async (done) => {
+    it("Success (alpha) determining the project version from file", (done) => {
         let testPath = path.join(__dirname, 'successAlphaDeterminingTheProjectVersionFromFile.js');
 
         let runner: mocktest.MockTestRunner = new mocktest.MockTestRunner(testPath);
 
-        await runner.runAsync();
+        runner.runAsync()
+            .then(() => {
+                assert.strictEqual(runner.succeeded, true);
+                assert.strictEqual(runner.invokedToolCount, 0);
+                assert(runner.stdOutContained('loc_mock_successGetUnityEditorVersion 2022.3.49a1, revision=4dae1bb8668d, alpha=true, beta=false'));
+                assert(runner.stdOutContained('##vso[task.setvariable variable=projectVersion;isOutput=false;issecret=false;]2022.3.49a1'));
+                assert(runner.stdOutContained('##vso[task.setvariable variable=projectVersionRevision;isOutput=false;issecret=false;]4dae1bb8668d'));
 
-        assert.strictEqual(runner.succeeded, true);
-        assert.strictEqual(runner.invokedToolCount, 0);
-        assert(runner.stdOutContained('loc_mock_successGetUnityEditorVersion 2022.3.49a1, revision=4dae1bb8668d, alpha=true, beta=false'));
-        assert(runner.stdOutContained('##vso[task.setvariable variable=projectVersion;isOutput=false;issecret=false;]2022.3.49a1'));
-        assert(runner.stdOutContained('##vso[task.setvariable variable=projectVersionRevision;isOutput=false;issecret=false;]4dae1bb8668d'));
-
-        done();
+                done();
+            });
     })
 
-    it("Success (beta) determining the project version from file", async (done) => {
+    it("Success (beta) determining the project version from file", (done) => {
         let testPath = path.join(__dirname, 'successBetaDeterminingTheProjectVersionFromFile.js');
 
         let runner: mocktest.MockTestRunner = new mocktest.MockTestRunner(testPath);
 
-        await runner.runAsync();
+        runner.runAsync()
+            .then(() => {
+                assert.strictEqual(runner.succeeded, true);
+                assert.strictEqual(runner.invokedToolCount, 0);
+                assert(runner.stdOutContained('loc_mock_successGetUnityEditorVersion 2022.3.49b1, revision=4dae1bb8668d, alpha=false, beta=true'));
+                assert(runner.stdOutContained('##vso[task.setvariable variable=projectVersion;isOutput=false;issecret=false;]2022.3.49b1'));
+                assert(runner.stdOutContained('##vso[task.setvariable variable=projectVersionRevision;isOutput=false;issecret=false;]4dae1bb8668d'));
 
-        assert.strictEqual(runner.succeeded, true);
-        assert.strictEqual(runner.invokedToolCount, 0);
-        assert(runner.stdOutContained('loc_mock_successGetUnityEditorVersion 2022.3.49b1, revision=4dae1bb8668d, alpha=false, beta=true'));
-        assert(runner.stdOutContained('##vso[task.setvariable variable=projectVersion;isOutput=false;issecret=false;]2022.3.49b1'));
-        assert(runner.stdOutContained('##vso[task.setvariable variable=projectVersionRevision;isOutput=false;issecret=false;]4dae1bb8668d'));
-
-        done();
+                done();
+            });
     })
 
-    it("Success (stable) determining the project version from file", async (done) => {
+    it("Success (stable) determining the project version from file", (done) => {
         let testPath = path.join(__dirname, 'successStableDeterminingTheProjectVersionFromFile.js');
 
         let runner: mocktest.MockTestRunner = new mocktest.MockTestRunner(testPath);
 
-        await runner.runAsync();
+        runner.runAsync()
+            .then(() => {
+                assert.strictEqual(runner.succeeded, true);
+                assert.strictEqual(runner.invokedToolCount, 0);
+                assert(runner.stdOutContained('loc_mock_successGetUnityEditorVersion 2022.3.49f1, revision=4dae1bb8668d, alpha=false, beta=false'));
+                assert(runner.stdOutContained('##vso[task.setvariable variable=projectVersion;isOutput=false;issecret=false;]2022.3.49f1'));
+                assert(runner.stdOutContained('##vso[task.setvariable variable=projectVersionRevision;isOutput=false;issecret=false;]4dae1bb8668d'));
 
-        assert.strictEqual(runner.succeeded, true);
-        assert.strictEqual(runner.invokedToolCount, 0);
-        assert(runner.stdOutContained('loc_mock_successGetUnityEditorVersion 2022.3.49f1, revision=4dae1bb8668d, alpha=false, beta=false'));
-        assert(runner.stdOutContained('##vso[task.setvariable variable=projectVersion;isOutput=false;issecret=false;]2022.3.49f1'));
-        assert(runner.stdOutContained('##vso[task.setvariable variable=projectVersionRevision;isOutput=false;issecret=false;]4dae1bb8668d'));
-
-        done();
+                done();
+            });
     })
 })
